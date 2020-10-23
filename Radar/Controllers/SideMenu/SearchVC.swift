29bab4e1a350e5a01f1c82ee4af6244e0ca2da92
happy guleria <37120434+happyguleria1234@users.noreply.github.com>
2020-10-23 18:23:26 +0530

    worked on designs

---
+++ Radar/Controllers/SideMenu/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  Radar
+//
+//  Created by Vivek Dharmani on 24/10/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
