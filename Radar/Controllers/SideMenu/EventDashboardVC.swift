1a82eac6694a4bf8fc6dbe59a92f610ce5f38099
happy guleria <37120434+happyguleria1234@users.noreply.github.com>
2020-10-27 18:56:36 +0530

    worked on designs

---
+++ Radar/Controllers/SideMenu/EventDashboardVC.swift
@@ -0,0 +1,20 @@
+//
+//  EventDashboardVC.swift
+//  Radar
+//
+//  Created by Vivek Dharmani on 27/10/20.
+//
+
+import UIKit
+
+class EventDashboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
